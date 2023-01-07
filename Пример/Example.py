8b85302f0f89d954ef32cc11610297d0d355c576
Мальцев Николай <Malzewn@gmail.com>
2023-01-07 19:20:29 +0300

    Выполнил первое индивидульное задание

---
+++ Пример/Example.py
@@ -0,0 +1,7 @@
+import os
+
+print("The keys and values of all environment variables:")
+for key in os.environ:
+    print(key, '=>', os.environ[key])
+
+print("The value of HOME is: ", os.environ['HOME'])
